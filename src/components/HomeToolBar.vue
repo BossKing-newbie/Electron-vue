b7298dcf0e7a11bfa6d7ee862e50c4020cf8c9aa
Guanyaowei <1210792729@qq.com>
2020-06-02 17:52:21 +0800

    修改用户手册

--- src/components/HomeToolBar.vue
+++ src/components/HomeToolBar.vue
@@ -19,6 +19,29 @@
         <!--用户手册编辑区-->
         <h4>用户手册</h4>
         <el-divider></el-divider>
+        <span style="font-family: '华文楷体'; font-size: 18px;">感谢您对快递屋的支持和关注!
+                <br>很高兴能为您提供服务!
+                <br>希望我们的服务能让您满意!
+                <br><br/>
+        </span>
+        <table width="315" border="0" style="text-align: right; text-decoration: blink; font-size: 16px; font-family: '楷体';">
+          <tr>
+            <td width="118" style="text-align: right">个人信息：</td>
+            <td width="187" style="text-align: left">查看与修改信息</td>
+          </tr>
+          <tr>
+            <td style="text-align: right">客户服务：</td>
+            <td style="text-align: left">下单选择产品</td>
+          </tr>
+          <tr>
+            <td style="text-align: right">订单管理：</td>
+            <td style="text-align: left">物流信息管理与历史订单</td>
+          </tr>
+          <tr>
+            <td style="text-align: right">联系我们：</td>
+            <td style="text-align: left">随时点击咨询疑问</td>
+          </tr>
+        </table>
       </el-drawer>
     </el-row>
   </el-header>
